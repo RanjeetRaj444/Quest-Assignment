5278865ddab964cd7ba797ff39a95832ec26ea84
ranjeetraj444 <ranjeetraj44666@gmail.com>
2024-02-17 22:38:53 +0530

    Folder and file setup completed

---
+++ questassignment/src/pages/Error.jsx
@@ -0,0 +1,16 @@
+export default function ErrorPage() {
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+				padding: "2rem",
+			}}
+		>
+			<h1>Page Not found!</h1>
+			<p>404 error!</p>
+		</div>
+	);
+}
